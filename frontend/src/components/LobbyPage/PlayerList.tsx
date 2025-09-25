fb42de0254582f813d7639a6e7daf6db99c35881
maciek <oskar.pyrzak@gmail.com>
2025-09-25 23:15:58 +0200

    improve player list component

--- frontend/src/components/LobbyPage/PlayerList.tsx
+++ frontend/src/components/LobbyPage/PlayerList.tsx
@@ -1,3 +1,5 @@
+import { Crown } from "lucide-react";
+
 type Player = {
   id: string;
   username: string;
@@ -5,22 +7,30 @@ type Player = {
 
 type PlayerListProps = {
   players: Player[];
+  ownerId: string;
   maxPlayers?: number;
 };
 
-export const PlayerList = ({ players, maxPlayers = 4 }: PlayerListProps) => {
+export const PlayerList = ({
+  players,
+  ownerId,
+  maxPlayers = 4,
+}: PlayerListProps) => {
   return (
     <div className="flex flex-col gap-4 w-full max-w-md mx-auto">
       {players.map((player) => (
         <div
           key={player.id}
-          className="relative flex justify-center items-center rounded-xl 
+          className="relative flex justify-between items-center rounded-xl 
                      ring-2 ring-violet-400 px-4 py-2 overflow-hidden"
         >
           <div className="absolute inset-0 bg-violet-900/10 backdrop-blur-xl rounded-xl"></div>
 
-          <div className="relative text-2xl font-semibold text-violet-100">
+          <div className="relative text-2xl font-semibold text-violet-100 flex items-center gap-2">
             {player.username}
+            {player.id === ownerId && (
+              <Crown className="w-5 h-5 text-yellow-400 drop-shadow" />
+            )}
           </div>
         </div>
       ))}
--- frontend/src/pages/LobbyPage.tsx
+++ frontend/src/pages/LobbyPage.tsx
@@ -1,17 +1,38 @@
+import { useEffect } from "react";
 import { Code } from "../components/LobbyPage/Code";
 import { PlayerList } from "../components/LobbyPage/PlayerList";
 import { useGameStore } from "../store/gameStore";
+import { useSocketStore } from "../store/socketStore";
+import type { PlayerType } from "../../../shared/types/types";
 
 export const LobbyPage = () => {
   const code = useGameStore((store) => store.code);
   const players = useGameStore((store) => store.players);
+  const setPlayers = useGameStore((store) => store.setPlayers);
+  const ownerId = useGameStore((store) => store.ownerId);
+
+  const socket = useSocketStore((store) => store.socket);
+
+  useEffect(() => {
+    if (!socket?.active) return;
+
+    const handlePlayerJoined = (player: PlayerType) => {
+      setPlayers([...players, { id: player.id, username: player.username }]);
+    };
+
+    socket.on("playerJoined", handlePlayerJoined);
+
+    return () => {
+      socket.off("playerJoined", handlePlayerJoined);
+    };
+  }, [socket, players, setPlayers]);
 
   return (
     <div className="h-screen w-screen py-[5vh]  relative flex flex-col items-center justify-center">
       <div className="absolute top-[5vh]">
         <Code code={code} />
       </div>
-      <PlayerList players={players} />
+      <PlayerList players={players} ownerId={ownerId} />
     </div>
   );
 };
