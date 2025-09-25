5a4be13eb8d4052ef0b0ca946ea77798430a39b0
maciek <oskar.pyrzak@gmail.com>
2025-09-25 23:16:43 +0200

    feat(createPlayer): Random Nicknames

--- server/src/game/player.ts
+++ server/src/game/player.ts
@@ -1,10 +1,48 @@
 import { AppSocket } from "..";
 import { PlayerType } from "../../../shared/types/types";
 
+const commonNames = [
+  "Josh",
+  "Emily",
+  "Michael",
+  "Jessica",
+  "Daniel",
+  "Ashley",
+  "Matthew",
+  "Sarah",
+  "David",
+  "Amanda",
+  "James",
+  "Jennifer",
+  "John",
+  "Elizabeth",
+  "Ryan",
+  "Samantha",
+  "Andrew",
+  "Lauren",
+  "Justin",
+  "Megan",
+  "Brandon",
+  "Nicole",
+  "Joseph",
+  "Stephanie",
+  "Anthony",
+  "Rachel",
+  "Kyle",
+  "Amber",
+  "Alexander",
+  "Heather",
+];
+
+const getRandomName = () => {
+  const index = Math.floor(Math.random() * commonNames.length);
+  return commonNames[index];
+};
+
 export const createPlayer = (socket: AppSocket): PlayerType => {
   const player: PlayerType = {
     id: socket.id,
-    username: "Placeholder",
+    username: getRandomName(),
     hand: [],
   };
 
